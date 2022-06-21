fa5fb9f54f84ae2584a5e222411092fbeed07215
Tatyana Litvinenko <102348502+Egirra@users.noreply.github.com>
2022-06-21 23:05:01 +0800

    addead fixes

--- src/main/java/homework/RequestCats.java
+++ src/main/java/homework/RequestCats.java
@@ -7,14 +7,14 @@ public class RequestCats {
     private final String text; //сообщение
     private final String type; //тип животного
     private final String user; //имя пользователя
-    private final String upvotes; //голоса
+    private final int upvotes; //голоса
 
     public RequestCats(
             @JsonProperty("id") String id,
             @JsonProperty("text") String text,
             @JsonProperty("type") String type,
             @JsonProperty("user") String user,
-            @JsonProperty("upvotes") String upvotes) {
+            @JsonProperty("upvotes") int upvotes) {
         this.id = id;
         this.text = text;
         this.type = type;
@@ -38,7 +38,7 @@ public class RequestCats {
         return user;
     }
 
-    public String getUpvotes() {
+    public int getUpvotes() {
         return upvotes;
     }
 
